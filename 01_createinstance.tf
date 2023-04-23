c571ca9ebe7bb54d67444ec297d63860592cba01
PyV!V!D <94853400+pyvivid@users.noreply.github.com>
2023-04-23 16:35:24 +0530

    with creds

--- 01_createinstance.tf
+++ 01_createinstance.tf
@@ -1,11 +1,11 @@
 provider "aws"{
     version = "4.64.0"
     region = "us-west-2"
-    access_key = ADGHJKKDN8976GGYH
-    secret_ket = /ui09djhewfjfkjlsslsaddhhkdkskka=
+    access_key = AKIA35XN7SLMWAEPLLUA
+    secret_ket = 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
 }
 
 resource "aws_instance" "my-first-aws-instance" {
-    ami = "ami-069aabeee6f53e7bf"
+    ami = "ami-0db245b76e5c21ca1"
     instance_type = "t2.micro"
 }
