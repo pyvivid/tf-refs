3205bdb0e2a55dc6257e78b3d8ebbe3c127aa739
PyV!V!D <94853400+pyvivid@users.noreply.github.com>
2023-04-26 13:21:06 +0530

    Update 02_provider.tf

--- 02_provider.tf
+++ 02_provider.tf
@@ -1,5 +1,8 @@
-provider "aws" {
+# Best practice to save the keys is in a dedicated secrets file.
+# file 3 will show use of env variable for access keys.
+provider "aws"{
+    version = "4.64.0"
     region = "us-west-2"
-    #version = "4.64.0"
-    
+    #access_key = "Enter_UR_Access_Key_Here"
+    #secret_ket = "Enter_your_secret_key_here"
 }
