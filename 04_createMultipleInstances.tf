5ad666e95abe611fcfc67bcef16e03e8b207694b
PyV!V!D <94853400+pyvivid@users.noreply.github.com>
2023-04-26 17:33:12 +0530

    Update 04_createMultipleInstances.tf

--- 04_createMultipleInstances.tf
+++ 04_createMultipleInstances.tf
@@ -2,4 +2,9 @@ resource "aws_instance" "my-first-aws-instance" {
     ami = "ami-0db245b76e5c21ca1"
     instance_type = "t2.micro"
     count = 3
+
+    tags = {
+        # Below will create 3 instances, each with unique names
+        Name = "demoInstance-${count.index}"
+    }
 }
