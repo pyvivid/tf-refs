eacd44a4aaf8611e1a4a6fd5d18b074dd9d655d6
PyV!V!D <94853400+pyvivid@users.noreply.github.com>
2023-04-28 21:48:19 +0530

    Update 05_tf_vars_usage.tf

--- 05_tf_vars_usage.tf
+++ 05_tf_vars_usage.tf
@@ -12,19 +12,18 @@ resource "aws_instance" "my-first-aws-instance" {
 # Create file2: provider.tf
 provider "aws" {
     access_key = "${var.AWS_ACCESS_KEY}"
-    secret_key = "${var.AWS_SECRET_OF_ACCESS}"
+    secret_key = "${var.AWS_SECRET_KEY}"
     region = "${var.AWS_REGION}"
 }
 
 # Create file3: variables.tf
 variable "AWS_ACCESS_KEY" {}
-variable "AWS_SECRET_OF_ACCESS" {}
+variable "AWS_SECRET_KEY" {}
 variable "AWS_REGION" {
     default = "us-west-2"
 }
 
 # Create file4: terraform.tfvars
 # ensure this file is added to git.ignore and not sync to git repo.
-AWS_ACCES_KEY = "enter_access_key_here"
-AWS_SECRET_OF_ACCESS = "enter_secret_key_here"
-AWS_REGION = "us-west-2"
+AWS_ACCESS_KEY="enter_access_key_here"
+AWS_SECRET_KEY="enter_secret_key_here"
