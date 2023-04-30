6034407b40d17ef44b785cf16cf56c1ae4680d4f
PyV!V!D <94853400+pyvivid@users.noreply.github.com>
2023-04-30 10:53:59 +0530

    Update 01_createinstance.tf

--- 01_createinstance.tf
+++ 01_createinstance.tf
@@ -2,7 +2,7 @@ provider "aws"{
     version = "4.64.0"
     region = "us-west-2"
     access_key = "Enter_UR_Access_Key_Here"
-    secret_ket = "Enter_your_secret_key_here"
+    secret_key = "Enter_your_secret_key_here"
 }
 
 resource "aws_instance" "my-first-aws-instance" {
